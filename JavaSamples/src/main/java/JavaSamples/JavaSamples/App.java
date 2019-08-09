518584f129726adb0c5a98644861b34d9d85d367
Sheeba <sheebu017@gmail.com>
2019-08-08 19:48:07 -0700

    Updating

--- JavaSamples/src/main/java/JavaSamples/JavaSamples/App.java
+++ JavaSamples/src/main/java/JavaSamples/JavaSamples/App.java
@@ -1,13 +1,20 @@
 package JavaSamples.JavaSamples;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	final static String BRANCH="MASTER";
+	
+	
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! " +BRANCH+ " "+ new Date());
     }
 }
