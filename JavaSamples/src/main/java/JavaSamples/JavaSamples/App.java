299cdf31b65f9c214d0cbbdad1ff1f52cd65c5a2
Sheeba <sheebu017@gmail.com>
2019-08-08 20:41:38 -0700

    committing

--- JavaSamples/src/main/java/JavaSamples/JavaSamples/App.java
+++ JavaSamples/src/main/java/JavaSamples/JavaSamples/App.java
@@ -16,5 +16,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! " +BRANCH+ " "+ new Date());
+        for (int i=0;i< 10;i++) {
+            System.out.println( " Counting ");
+            System.out.println( i);
+
+
+        }
     }
 }
